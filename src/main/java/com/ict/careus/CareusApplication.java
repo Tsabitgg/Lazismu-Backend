55a8c989a8303ca8418f063d4f9d9c8a8c5dc1b1
Tsabit Gholib <tsabit1502@gmail.com>
2024-05-27 18:12:31 +0700

    configure war

--- src/main/java/com/ict/careus/CareusApplication.java
+++ src/main/java/com/ict/careus/CareusApplication.java
@@ -2,9 +2,12 @@ package com.ict.careus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class CareusApplication {
+@ComponentScan
+public class CareusApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareusApplication.class, args);
