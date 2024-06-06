d87c6aa9246409ab4d83b319a8cbb2320ec9e2a9
Tsabit Gholib <tsabit1502@gmail.com>
2024-06-06 23:06:30 +0700

    add

--- src/main/java/com/ict/careus/repository/SubmissionRepository.java
+++ src/main/java/com/ict/careus/repository/SubmissionRepository.java
@@ -1,5 +1,6 @@
 package com.ict.careus.repository;
 
+import com.ict.careus.model.campaign.Campaign;
 import com.ict.careus.model.transaction.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
 
-    List<Submission> findByApproved(boolean approved)
-;}
+    List<Submission> findByApproved(boolean approved);
+    List<Submission> findAllByCampaign(Campaign campaign);
+}
