9c61e6a637cc8268aae6052b4bc0cf9af2f700aa
Tsabit Gholib <tsabit1502@gmail.com>
2024-06-25 22:22:51 +0700

    FIX CORS

--- src/main/java/com/ict/careus/CareusApplication.java
+++ src/main/java/com/ict/careus/CareusApplication.java
@@ -2,14 +2,18 @@ package com.ict.careus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class CareusApplication{
+public class CareusApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CareusApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareusApplication.class, args);
 	}
-
 }
